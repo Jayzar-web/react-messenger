6e45a8fc3788983490e02df8fcd4014b041b218a
ShineRookie <76092693+ShineRookie@users.noreply.github.com>
2024-05-03 18:56:08 +0500

    change actions

--- src/firebase.js
+++ src/firebase.js
@@ -6,9 +6,9 @@ import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  authDomain: 'flux-messenger-58c3e.firebaseapp.com',
+  projectId: 'flux-messenger-58c3e',
+  storageBucket: 'flux-messenger-58c3e.appspot.com',
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
